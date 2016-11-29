42d49aec8e4d8d3e6e27d4491310612394c289f4
ThomasKenyeres <kenyerest97@gmail.com>
2016-11-29 13:54:15 +0100

    table import error handling moved to common.py

--- common.py
+++ common.py
@@ -18,3 +18,16 @@ def generate_random(table):
     # your code
 
     return generated
+
+def check_rows(table, title_list):
+    row_len = 0
+    for row in table:
+        if row_len == 0:
+            for data in row:
+                row_len += 1
+        elif row_len == len(row):
+            pass
+        else:
+            raise IndexError("Difference in rows' length")
+    if not len(title_list) == row_len:
+        raise IndexError("Title list and rows' length are different")
